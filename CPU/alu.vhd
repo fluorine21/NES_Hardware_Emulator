f53d8c6c62dd19d16bdf099879c4920782204d08
Megs  Yadav <megsyadav@Megss-MacBook-Pro.local>
2020-05-06 16:15:36 -0400

    alu additions and testbench edits

--- CPU/alu.vhd
+++ CPU/alu.vhd
@@ -1,18 +1,17 @@
-
 library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
 
 entity alu is
     port(
-		inputA : in std_logic_vector(7 downto 0);
+	inputA : in std_logic_vector(7 downto 0);
         inputB: in std_logic_vector(7 downto 0);
         alu_op: in std_logic_vector(2 downto 0);
         opcode : in std_logic_vector(7 downto 0);
         proc_status_in : in std_logic_vector(7 downto 0);
         proc_status_edit : in std_logic_vector(7 downto 0);
         
-        ignore_ouput : out std_logic;
+        ignore_output : out std_logic;
         proc_status_out : out std_logic_vector(7 downto 0);
         alu_out : out std_logic_vector(7 downto 0)
 
@@ -21,85 +20,95 @@ end entity alu;
 
 architecture a of alu is 
 
-    signal temp_output : std_logic_vector(7 downto 0);
-    signal unsigned_out : unsigned(7 downto 0);
-    signal proc_status_temp : std_logic_vector(7 downto 0);
-    signal shift_carry : std_logic;
-    signal sbc_with_carry : unsigned(8 downto 0);
+    
+    
 
     begin
 
-    comb_process: process(alu_op, inputA, inputB) 
+    comb_process: process(alu_op, inputA, inputB, opcode, proc_status_in, proc_status_edit) 
 	function to_integer( s : std_logic ) return natural is
-begin
-  if s = '1' then
-    return 1;
-  else
-    return 0;
-  end if;
-end function;
+		begin
+	  if s = '1' then
+	    return 1;
+	  else
+	    return 0;
+	  end if;
+	end function;
+    variable sbc_with_carry : unsigned(8 downto 0) := (others => '0');
+    variable unsigned_out : unsigned(7 downto 0) := (others => '0');
+	variable proc_status_temp : std_logic_vector(7 downto 0);
+	variable temp_output : std_logic_vector(7 downto 0);
+
     begin
-    proc_status_temp <= proc_status_in; ----set new proc status to old proc status then edit what you are allowed to
+
+    proc_status_temp := proc_status_in; ----set new proc status to old proc status then edit what you are allowed to
+    ignore_output <= '0';
 
     case (alu_op) is
         when "000" => -------add
             if (opcode = x"00") then ---add with carry
 
-                unsigned_out <= unsigned(inputA) + unsigned(inputB) + to_integer(proc_status_in(0));
-                temp_output <= std_logic_vector(unsigned_out);
+                unsigned_out := unsigned(inputA) + unsigned(inputB) + to_integer(proc_status_in(0));
+                temp_output := std_logic_vector(unsigned_out);
                 if (unsigned_out > to_unsigned(255,8) and proc_status_in(3) = '0') or (unsigned_out > to_unsigned(99,8) and proc_status_in(3) = '1') then
-                    proc_status_temp(3) <= '1'; ------ setting carry flag
+                    proc_status_temp(0) := '1'; ------ setting carry flag
                 end if;
                 -------------set overflow flag---------
             else ---inc,inx,iny all just increment and pla and plp pops PC, 
-                temp_output <= std_logic_vector(unsigned(inputA) + to_unsigned(1,8));
+                temp_output := std_logic_vector(unsigned(inputA) + to_unsigned(1,8));
             end if;
 
         when "001" => -----sub
         ---dec, dex,dey, jsr, pha,php
             if (opcode = x"14" or opcode = x"15" or opcode = x"16"
             or opcode = x"24" or opcode = x"25" or opcode = x"1D") then
-                temp_output <= std_logic_vector(unsigned(inputA) - to_unsigned(1,8));
-            ------ cpx, cpy, cmp
+                temp_output := std_logic_vector(unsigned(inputA) - to_unsigned(1,8));
+            ------ cpx, cpy, cmp inputA is from acc and input B from memory
             elsif (opcode = x"11" or opcode = x"12" or opcode = x"13") then
-                temp_output <= std_logic_vector(unsigned(inputA) - unsigned(inputB));
+                temp_output := std_logic_vector(unsigned(inputA) - unsigned(inputB));
+		if unsigned(inputA) >= unsigned(inputB) then
+			proc_status_temp(0) := '1'; --------carry flag
+		else 
+			proc_status_temp(0) := '0';		
+		end if;
             else ----- sbc
-                sbc_with_carry <= unsigned(inputA);
-                if unsigned(inputA) > unsigned(inputB) then
-                    sbc_with_carry <= unsigned(inputA) + to_unsigned(256,9);
-                    proc_status_temp(3) <= '0' ;------ reseting the carry flag
+                sbc_with_carry := resize(unsigned(inputA),9);
+
+                if unsigned(inputA) < unsigned(inputB) then
+                    sbc_with_carry := unsigned(inputA) + to_unsigned(256,9);
+                    proc_status_temp(0) := '0' ;------ reseting the carry flag
                 end if;
-                temp_output <= std_logic_vector(sbc_with_carry - unsigned(inputB));
+                temp_output := std_logic_vector(resize((sbc_with_carry - unsigned(inputB)),8));
             end if;
             
         when "010" => ------shift
             if  (opcode = x"28") then--- rotate left
-                temp_output <= inputA(6 downto 0) & proc_status_in(3);
-                proc_status_temp(3) <= inputA(7); ----- carry flag
+                temp_output := inputA(6 downto 0) & proc_status_in(3);
+                proc_status_temp(0) := inputA(7); ----- carry flag
             elsif  (opcode = x"29") then --- rotate right
-                temp_output <= proc_status_in(3) & inputA(7 downto 1) ;
-                proc_status_temp(3) <= inputA(0); ----- carry flag
+                temp_output := proc_status_in(3) & inputA(7 downto 1) ;
+                proc_status_temp(0) := inputA(0); ----- carry flag
             elsif  (opcode = x"02") then--- arithmetic shift left
-                proc_status_temp(3) <= inputA(7); --- carry flag
-                temp_output <= inputA(6 downto 0) & '0';
+                proc_status_temp(0) := inputA(7); --- carry flag
+                temp_output := inputA(6 downto 0) & '0';
             elsif  (opcode = x"21") then--- logical shift right
-                proc_status_temp(3) <= inputA(0); ----- carry flag
-                temp_output <= '0' & inputA(7 downto 1) ;
+                proc_status_temp(0) := inputA(0); ----- carry flag
+                temp_output := '0' & inputA(7 downto 1) ;
             end if;
 
         when "011" => ----- and
-            temp_output <= inputA and inputB;
+            temp_output := inputA and inputB;
 
         when "100" => ----- or 
 
-            temp_output <= inputA or inputB;
+            temp_output := inputA or inputB;
 
         when "101" => ------ xor
-            temp_output <= inputA xor inputB;
+            temp_output := inputA xor inputB;
 
         when OTHERS => 
-            temp_output <= inputA;
-            ignore_ouput <= '1';
+            temp_output := inputA;
+            ignore_output <= '1';
     end case;
 
 
@@ -108,30 +117,46 @@ end function;
     if proc_status_edit(1) = '1' then   ---- zero (1)
         
         if temp_output = "00000000" or opcode = x"21" then 
-            proc_status_temp(1) <= '1';
+            proc_status_temp(1) := '1';
         else
-            proc_status_temp(1) <= '0';
+            proc_status_temp(1) := '0';
         end if;
     end if;
 
-    if proc_status_edit(7) = '1' then   ---- negative(7), carry(3)
+    if proc_status_edit(7) = '1' then   ---- negative(7), carry(0)
         if (opcode = x"03") then ------ BIT
-            proc_status_temp(7) <= inputB(7) ;----------n(7) bit 7 of byte in memory
-            proc_status_temp(6) <= inputB(6) ; -----------v(6) bit 6 of byte in memory
-            ignore_ouput <= '1';
+            proc_status_temp(7) := inputB(7) ;----------n(7) bit 7 of byte in memory
+            proc_status_temp(6) := inputB(6) ; -----------v(6) bit 6 of byte in memory
+            ignore_output <= '1';
         elsif temp_output(7) = '1' then 
-            proc_status_temp(7) <= '1';
+            proc_status_temp(7) := '1';
         else
-            proc_status_temp(7) <= '0';
+            proc_status_temp(7) := '0';
         end if;
     end if;
 
+	if (opcode = x"0D") then  -- CLC
+		proc_status_temp(0) := '0';
+	elsif (opcode = x"0E") then --CLD
+		proc_status_temp(3) := '0';
+	elsif (opcode = x"0F") then --CLI
+		proc_status_temp(2) := '0';
+	elsif (opcode = x"10") then --CLV
+		proc_status_temp(6) := '0';
+	elsif (opcode = x"2E") then --SEC
+		proc_status_temp(0) := '1';
+	elsif (opcode = x"30") then --SEI
+		proc_status_temp(2) := '1';
+	elsif (opcode = x"2F") then  --SED
+		proc_status_temp(3) := '1';
+	end if;
     
 
 
-    alu_out <= temp_output;
     proc_status_out <= proc_status_temp;
+    alu_out <= temp_output;
     end process;
+   
     
 
 end architecture a;
--- CPU/alu_tb.vhd
+++ CPU/alu_tb.vhd
@@ -15,7 +15,7 @@ architecture behavioral of alu_tb is
         proc_status_in : in std_logic_vector(7 downto 0);
         proc_status_edit : in std_logic_vector(7 downto 0);
         
-        ignore_ouput : out std_logic;
+        ignore_output : out std_logic;
         proc_status_out : out std_logic_vector(7 downto 0);
         alu_out : out std_logic_vector(7 downto 0)
         );
@@ -28,7 +28,7 @@ architecture behavioral of alu_tb is
     signal proc_status_in :  std_logic_vector(7 downto 0);
     signal proc_status_edit :  std_logic_vector(7 downto 0);
         
-    signal ignore_ouput :  std_logic;
+    signal ignore_output :  std_logic;
     signal proc_status_out :  std_logic_vector(7 downto 0);
     signal alu_out :  std_logic_vector(7 downto 0);
 	
@@ -42,37 +42,96 @@ architecture behavioral of alu_tb is
             proc_status_in => proc_status_in,
             proc_status_edit =>  proc_status_edit,
             
-            ignore_ouput  => ignore_ouput,
+            ignore_output  => ignore_output,
             proc_status_out  => proc_status_out,
             alu_out  => alu_out
 		); 
 
-	clk_process : process is
-	begin	
-		clk <= '0';
-		wait for 1 ns;
-		for i in 0 to 5 loop
-            clk <= '1';
-            wait for 5 ns;
-            clk <= '0';
-            wait for 5 ns;
-		end loop;
-		wait;
-	end process;
+
 	
 	alu_process : process is
 	begin
-		wait for 1 ns;
-		inputA <= "10101010"
-        inputB <= "10101010"
-        alu_op <= "000"
-        opcode <= "00011010"
-        proc_status_in <= "00000000"
-        proc_status_edit <= "11111111"
-            
-        wait for 5 ns;
-		
-		wait;
+	-- INX -- check negative flag
+	inputA <= "01111111";
+    inputB <= "00000000";
+    alu_op <= "000";
+    opcode <= x"1A";
+    proc_status_in <= "00000000";
+    proc_status_edit <= "11111111";
+ 	wait for 5 ns;
+	--DEX -- check zero flag
+	inputA <= "00000001";
+    inputB <= "00000000";
+    alu_op <= "001";
+    opcode <= x"15";
+    proc_status_in <= "00000000";
+    proc_status_edit <= "11111111";
+ 	wait for 5 ns;
+	--SBC with carry
+	inputA <= "00000011";
+    inputB <= "00000100";
+    alu_op <= "001";
+    opcode <= x"2D";
+    proc_status_in <= "00000001";
+    proc_status_edit <= "11111111";
+	wait for 5 ns;
+	--SBC without carry
+	inputB <= "00000001";
+	wait for 5 ns;
+	--ADC with carry
+	opcode <= x"00";
+	alu_op <= "000";
+	wait for 5 ns;
+	--ADC without carry
+	proc_status_in <= "00000000";
+	wait for 5 ns;
+	--ADC with overflow, no carry
+	inputA <= "11111011";
+	inputB <= "00001000";
+	wait for 5 ns;
+	--ADC with overflow and carry
+	proc_status_in <= "00000001";
+	wait for 5 ns;
+	--BIT - make sure ignore output flag on
+	opcode <= x"03";
+	alu_op <= "011";
+	inputA <= "11110000";
+	inputB <= "10101010";
+	wait for 5 ns;
+    	--CPX
+	proc_status_in <= "00000000";
+  	proc_status_edit <= "11111111";
+	alu_op <=  "001";
+	opcode <= x"11";
+	inputA <= "01101100";
+	inputB <= "00001011";
+	wait for 5 ns;
+	inputA <= "00000001";
+	wait for 5 ns;
+	--OR
+	inputA <= "11110000";
+	inputB <= "10101010";
+	alu_op <= "100";
+	wait for 5 ns;
+	--XOR
+	alu_op <= "101";
+	wait for 5 ns;
+	--ROL
+	inputA <= "10101010";
+	alu_op <= "010";
+	opcode <= x"28";
+	wait for 5 ns;
+	--ROR
+	opcode <= x"29";
+	wait for 5 ns;
+	--ASL
+	opcode <= x"02";
+	wait for 5 ns;
+	--LSR
+	opcode <= x"21";
+ 	wait for 5 ns;
+	wait;
+	
 
 	end process;
 end architecture;
