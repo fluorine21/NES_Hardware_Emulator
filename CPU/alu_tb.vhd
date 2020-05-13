3e3b5dfcee0b19607320227f4f64c8a3c0ec4efe
Megs  Yadav <megsyadav@Megss-MacBook-Pro.local>
2020-05-13 17:55:04 -0400

    ALU is done

--- CPU/alu_tb.vhd
+++ CPU/alu_tb.vhd
@@ -1,6 +1,8 @@
 library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
+library work;
+use work.instr_fetch_pkg.all;
 
 entity alu_tb is
 end entity alu_tb;
@@ -10,7 +12,7 @@ architecture behavioral of alu_tb is
 		port(
 		inputA : in std_logic_vector(7 downto 0);
         inputB: in std_logic_vector(7 downto 0);
-        alu_op: in std_logic_vector(2 downto 0);
+        alu_op: in std_logic_vector(3 downto 0);
         opcode : in std_logic_vector(7 downto 0);
         proc_status_in : in std_logic_vector(7 downto 0);
         proc_status_edit : in std_logic_vector(7 downto 0);
@@ -23,7 +25,7 @@ architecture behavioral of alu_tb is
 	
 	signal inputA : std_logic_vector(7 downto 0);
     signal inputB:  std_logic_vector(7 downto 0);
-    signal alu_op:  std_logic_vector(2 downto 0);
+    signal alu_op:  std_logic_vector(3 downto 0);
     signal opcode :  std_logic_vector(7 downto 0);
     signal proc_status_in :  std_logic_vector(7 downto 0);
     signal proc_status_edit :  std_logic_vector(7 downto 0);
@@ -53,34 +55,34 @@ architecture behavioral of alu_tb is
 	begin
 	-- INX -- check negative flag
 	inputA <= "01111111";
-    inputB <= "00000000";
-    alu_op <= "000";
-    opcode <= x"1A";
-    proc_status_in <= "00000000";
-    proc_status_edit <= "11111111";
+        inputB <= "00000000";
+        alu_op <= ADD_OP;
+        opcode <= x"1A";
+        proc_status_in <= "00000000";
+        proc_status_edit <= "11111111";
  	wait for 5 ns;
 	--DEX -- check zero flag
 	inputA <= "00000001";
-    inputB <= "00000000";
-    alu_op <= "001";
-    opcode <= x"15";
-    proc_status_in <= "00000000";
-    proc_status_edit <= "11111111";
+        inputB <= "00000000";
+        alu_op <= SUB_OP;
+        opcode <= x"15";
+        proc_status_in <= "00000000";
+        proc_status_edit <= "11111111";
  	wait for 5 ns;
 	--SBC with carry
 	inputA <= "00000011";
-    inputB <= "00000100";
-    alu_op <= "001";
-    opcode <= x"2D";
-    proc_status_in <= "00000001";
-    proc_status_edit <= "11111111";
+        inputB <= "00000100";
+        alu_op <= SUB_OP;
+        opcode <= x"2D";
+        proc_status_in <= "00000001";
+        proc_status_edit <= "11111111";
 	wait for 5 ns;
 	--SBC without carry
 	inputB <= "00000001";
 	wait for 5 ns;
 	--ADC with carry
 	opcode <= x"00";
-	alu_op <= "000";
+	alu_op <= ADD_OP;
 	wait for 5 ns;
 	--ADC without carry
 	proc_status_in <= "00000000";
@@ -94,14 +96,14 @@ architecture behavioral of alu_tb is
 	wait for 5 ns;
 	--BIT - make sure ignore output flag on
 	opcode <= x"03";
-	alu_op <= "011";
+	alu_op <= AND_OP;
 	inputA <= "11110000";
-	inputB <= "10101010";
+	inputB <= "11101010";
 	wait for 5 ns;
     	--CPX
 	proc_status_in <= "00000000";
   	proc_status_edit <= "11111111";
-	alu_op <=  "001";
+	alu_op <=  SUB_OP;
 	opcode <= x"11";
 	inputA <= "01101100";
 	inputB <= "00001011";
@@ -111,14 +113,15 @@ architecture behavioral of alu_tb is
 	--OR
 	inputA <= "11110000";
 	inputB <= "10101010";
-	alu_op <= "100";
+	alu_op <= OR_OP;
 	wait for 5 ns;
 	--XOR
-	alu_op <= "101";
+	alu_op <= XOR_OP;
 	wait for 5 ns;
 	--ROL
+	proc_status_in <= "00000001";
 	inputA <= "10101010";
-	alu_op <= "010";
+	alu_op <= SHIFT_OP;
 	opcode <= x"28";
 	wait for 5 ns;
 	--ROR
@@ -130,8 +133,91 @@ architecture behavioral of alu_tb is
 	--LSR
 	opcode <= x"21";
  	wait for 5 ns;
-	wait;
+	--INPUTA TO OUT
+	inputA <= "00000000";
+	inputB <= "10000000";
+	alu_op <="0110";
+	wait for 5 ns;
+	--INPUTB TO OUT;
+	alu_op <="0111";
+	wait for 5 ns;
+	---clear FLAGS
+	proc_status_in <= "11111111";
+        proc_status_edit <= "01111101";
+	alu_op <= "1000";
+	opcode <= x"0D";
+	wait for 5 ns;
+	opcode <= x"0E";
+	wait for 5 ns;
+	opcode <= x"0F";
+	wait for 5 ns;
+	opcode <= x"10";
+	wait for 5 ns;
+	proc_status_in <= "00000000";
+	wait for 5 ns;
+	--set flags;
+	opcode <= x"2E";
+	wait for 5 ns;
+	opcode <= x"2F";
+	wait for 5 ns;
+	opcode <= x"30";
+	wait for 5 ns;
+	proc_status_in <= "11111111";
+	wait for 5 ns;
+	--ADC and SBC carry checks
+	--SBC
+	inputA <= "00000011";
+        inputB <= "00000100";
+        alu_op <= SUB_OP;
+        opcode <= x"2D";
+        proc_status_in <= "00000001";
+        proc_status_edit <= "11111111";
+	wait for 5 ns;
+	inputA <= "11110000";
+	wait for 5 ns;
+	proc_status_in <= "00000000";
+	wait for 5 ns;
+	--ADC
+	opcode <= x"00";
+	alu_op <= ADD_OP;
+	inputA <= "00000011";
+	inputB <= "00001011";
+	proc_status_in <= "00000000";
+	wait for 5 ns;
+	proc_status_in <= "00000001";
+	wait for 5 ns;
+	inputB <= "11111110";
+	wait for 5 ns;
+	proc_status_in <= "00000000";
+	wait for 5 ns;
 	
+	--CHECK ADC OVERFLOW FLAG
+	inputA<= "00000011";	
+	inputB <="00010000";
+	wait for 5 ns;
+	inputB <= "11000000";
+	wait for 5 ns;
+	inputA <= "11100000";
+	inputB <= "00000011";
+	wait for 5 ns;
+	inputB <="01001111";
+	wait for 5 ns;
 
+	--CHECK SBC OVERFLOW FLAG
+	alu_op <= SUB_OP;
+        opcode <= x"2D";
+	proc_status_in <= "00000001";
+	proc_status_edit <= "11111111";
+	inputA <= "10000000";
+	inputB <= "00000100";
+	wait for 5 ns;
+	inputA <= "11111111";
+	wait for 5 ns;
+	inputA <= "00000100";
+	inputB <= "10000000";
+	wait for 5 ns;
+	inputB <= "11111111";
+	wait for 5 ns;
+	wait;
 	end process;
 end architecture;
